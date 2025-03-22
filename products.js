98a4bf9dfe17fbf91250089389ecc4047c48e801
Hector Baloyi <hectaville@live.com>
2025-03-22 07:36:13 +0200

    Update products.js

--- products.js
+++ products.js
@@ -24,6 +24,22 @@ const products = [
       image: "images/led_ceiling_light.jpg"
     },
     {
+      name: "Deco Ceiling Light",
+      category: "Electrical & Lighting",
+      subcategory: "Lighting",
+      description: "Stylish and modern decorative ceiling lights to enhance any space. Perfect for homes, offices, and commercial settings.",
+      price: "From R1200",
+      image: "images/decolite.jpg"
+    },
+    {
+      name: "Ceiling Light",
+      category: "Electrical & Lighting",
+      subcategory: "Lighting",
+      description: "Reliable and efficient ceiling lights for everyday illumination. Ideal for homes, offices, and commercial spaces.",
+      price: "R200",
+      image: "images/ceiling.jpg"
+    },
+    {
       name: "2.5mm Flat Twin and Earth Cable",
       category: "Electrical & Lighting",
       subcategory: "Lighting",
@@ -40,12 +56,28 @@ const products = [
       image: "images/2cord.jpg"
     },
     {
+      name: "100W LED Sport Light",
+      category: "Electrical & Lighting",
+      subcategory: "Lighting",
+      description: " High-performance sports lighting with bright, energy-efficient LED technology. Ideal for stadiums, courts, and outdoor areas.",
+      price: "R500",
+      image: "images/led.jpg"
+    },
+    {
       name: "Roof Tiles",
       category: "Building & Construction",
       subcategory: "Roofing",
       description: "Weather-resistant roof tiles for residential buildings.",
       price: "R25 per tile",
       image: "images/roof_tiles.jpg"
+    },
+    {
+      name: "Corner blocks",
+      category: "Building & Construction",
+      subcategory: "Roofing",
+      description: "Strong and durable blocks designed for reinforcing and finishing the corners of walls in construction projects.",
+      price: "R40 per pack of 40s",
+      image: "images/khona.jpg"
     },
     {
       name: "Standard Doors",
@@ -64,6 +96,62 @@ const products = [
       image: "images/Lockset.jpg"
     },
     {
+      name: "Blinds",
+      category: "Doors & Windows",
+      subcategory: "Window Blinds",
+      description: "Stylish and functional blinds for privacy and light control. Available in various designs and colors to suit any space.",
+      price: "From R450",
+      image: "images/blinds.jpg"
+    },
+    {
+      name: "Cutting Disc Steel",
+      category: "Tools & Equipment",
+      subcategory: "Tools",
+      description: "Precision-engineered for fast, clean cuts.",
+      price: "From R17",
+      image: "images/disk.jpg"
+    },
+    {
+      name: "Power Back-up Solutions",
+      category: "Services & Hire",
+      subcategory: "Services",
+      description: " Professional installation of backup power systems, including inverters, generators, and solar solutions. Ensure uninterrupted power for your home or business.",
+      price: "Contact Us For Prices",
+      image: "images/backup.jpg"
+    },
+    {
+      name: "Steel scaffolding Hire",
+      category: "Services & Hire",
+      subcategory: "Equipment Hire",
+      description: "Strong, stable, and ideal for construction and maintenance work. Flexible rental terms available",
+      price: "Hire From R400",
+      image: "images/Steel_Scaffolding.jpg"
+    },
+    {
+      name: "Power Generator Hire",
+      category: "Services & Hire",
+      subcategory: "Equipment Hire",
+      description: "Reliable generators for backup power during events, construction, or emergencies. Various sizes available.",
+      price: "Hire From R250",
+      image: "images/generator.jpg"
+    },
+    {
+      name: "Chairs For Hire",
+      category: "Services & Hire",
+      subcategory: "Equipment Hire",
+      description: " Affordable and sturdy chairs for events, meetings, and gatherings. Available in various styles to suit any occasion.",
+      price: "Hire From R5 per chair",
+      image: "images/chair.jpg"
+    },
+    {
+      name: "Tables For Hire",
+      category: "Services & Hire",
+      subcategory: "Equipment Hire",
+      description: " Affordable and sturdy tables for events, meetings, and gatherings. Available in various styles to suit any occasion.",
+      price: "Hire From R150",
+      image: "images/table.jpg"
+    },
+    {
       name: "Slab Blocks",
       category: "Building & Construction",
       subcategory: "Slabs",
@@ -72,12 +160,28 @@ const products = [
       image: "images/slab.jpg"
     },
     {
+      name: "Maxi Bricks",
+      category: "Building & Construction",
+      subcategory: "Bricks",
+      description: "Larger, high-strength bricks designed for faster construction and improved insulation, perfect for load-bearing walls.",
+      price: "R4 each",
+      image: "images/maxi.jpg"
+    },
+    {
+      name: "Stock Bricks",
+      category: "Building & Construction",
+      subcategory: "Bricks",
+      description: "Strong and durable clay bricks, ideal for general construction, internal walls, and structural work.",
+      price: "R2 each",
+      image: "images/Cement-Stock-Bricks-RSS.jpg"
+    },
+    {
       name: "Safety Boots",
       category: "Safety & Protective Gear",
       subcategory: "Safety & Protective Wear",
       description: "Heavy-duty safety boots with steel toe protection.",
-      price: "R850",
-      image: "images/safety_boots.jpg"
+      price: "R400",
+      image: "images/boots.jpg"
     }
   ];
   
